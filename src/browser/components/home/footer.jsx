3899a8af1fa354534bb1ea29fd97b355e7a04e93
Jessica Sperling <jessica_chen2@intuit.com>
2015-02-21 17:07:50 -0800

    landing page changes : minimized intro message picture & added footer with information

--- src/browser/components/home/footer.jsx
+++ src/browser/components/home/footer.jsx
@@ -7,21 +7,26 @@ module.exports = React.createClass({
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12">
-                            <ul className="list-inline">
-                                <li>
-                                    <a href="#">Home</a>
+                            <ul className="clearfix">
+                                <li className="col-sm-4">
+                                    <h3>Join Us</h3>
+                                    <ul>
+                                        <li className="text-muted small"> 9:00 AM Bible Study</li>
+                                        <li className="text-muted small"> 10:30 AM Main Worship ( English & Spanish )</li>
+                                        <li className="text-muted small"> 5:30 PM College & Young Adults Service</li>
+                                    </ul>
                                 </li>
-                                <li className="footer-menu-divider">&sdot;</li>
-                                <li>
-                                    <a href="#about">About</a>
+                                <li className="col-sm-4">
+                                    <h3>Location</h3>
+                                    <p className="text-muted small"> 875 South Park Victoria Drive, Milpitas CA 95035 </p>
                                 </li>
-                                <li className="footer-menu-divider">&sdot;</li>
-                                <li>
-                                    <a href="#services">Services</a>
-                                </li>
-                                <li className="footer-menu-divider">&sdot;</li>
-                                <li>
-                                    <a href="#contact">Contact</a>
+                                <li className="col-sm-4">
+                                    <h3>Contact Us</h3>
+                                    <ul>
+                                        <li className="text-muted small"> (408) 263-9000</li>
+                                        <li className="text-muted small"> info@parkvictoria.org </li>
+                                        <li className="text-muted small">(fb) (twitter)</li>
+                                    </ul>
                                 </li>
                             </ul>
                             <p className="copyright text-muted small">Copyright &copy; Park Victoria Baptist Church 2015. All Rights Reserved</p>
