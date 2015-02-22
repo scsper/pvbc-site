8337c587acf773a80878e4c8eb373a96446796f5
Scott Sperling <scsper@gmail.com>
2015-02-21 18:08:53 -0800

    a little bit of staff fit and finish

--- src/browser/components/staff/item.jsx
+++ src/browser/components/staff/item.jsx
@@ -8,7 +8,8 @@ StaffPage = React.createClass({
             <li className="staff-list-item">
                 <img className="staff-img" src={this.props.imageLink} />
                 <div className="staff-text">
-                    <h1 className="staff-name">{this.props.name}</h1>
+                    <h2 className="staff-name">{this.props.name}</h2>
+                    <h4 className="staff-title">{this.props.title}</h4>
                     <p className="staff-bio">
                         Cupcake ipsum dolor sit amet gingerbread. Chupa chups lollipop lollipop pastry. Candy biscuit powder. Chupa chups chocolate chupa chups. Ice cream danish bonbon oat cake cake jelly pudding topping bonbon. Lollipop soufflé cupcake pastry.
                         Ice cream cupcake icing tootsie roll. Danish gingerbread tootsie roll toffee gingerbread cheesecake sesame snaps lemon drops. Marzipan cookie sweet cotton candy donut sweet cotton candy. Cake cheesecake marzipan tart powder gummi bears. Sugar plum soufflé wafer oat cake pie. Topping jelly beans topping fruitcake marshmallow pie wafer.
